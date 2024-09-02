950de38ed9b92d53ac28977f2fd8de612bc2697b
SadiKISLAR <sadi22kislar@gmail.com>
2024-09-02 23:47:19 +0300

    circle chart added

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
@@ -5,14 +6,25 @@ const AdminPage = () => {
     <div className='p-4 flex gap-4 flex-col md:flex-row'>
 
       {/* LEFT */}
-      <div className="w-full lg:w-2/3">
-      {/* USER CARDS */}
-      <div className=" flex gap-4 justify-between flex-wrap">
+      <div className="w-full lg:w-2/3 flex flex-col md:flex-col gap-8">
+       {/* USER CARDS */}
+       <div className=" flex gap-4 justify-between flex-wrap">
         <UserCard type="student"/>
         <UserCard type="teacher"/>
         <UserCard type="parent"/>
         <UserCard type="staff"/>
-      </div>
+       </div>
+       {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+        {/* COUNT CHART */}
+        <div className="w-full lg:w-1/3  h-[450px]">
+        <CountChart/>
+        </div>
+        {/* ATTANDANCE CHART */}
+        <div className="w-full lg:w-2/3  h-[450px]"></div>
+       </div>
+       {/* BOTTOM CHARTS */}
+       
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3">r</div>
